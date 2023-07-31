3e745ea5fe29856a76a168b02c10b06d43849df5
Ayeza <ayezabashir442@gmail.com>
2023-07-31 23:57:00 +0500

    sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -2,6 +2,25 @@ import admin from '../../assets/images/adminimg.png'
 import SettingsIcon from '@mui/icons-material/Settings';
 import PowerSettingsNewIcon from '@mui/icons-material/PowerSettingsNew';
 import DashboardIcon from '@mui/icons-material/Dashboard';
+import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import ContentPasteIcon from '@mui/icons-material/ContentPaste';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import ImportContactsIcon from '@mui/icons-material/ImportContacts';
+import PersonAddAlt1Icon from '@mui/icons-material/PersonAddAlt1';
+import NoteAddIcon from '@mui/icons-material/NoteAdd';
+import ViewArrayIcon from '@mui/icons-material/ViewArray';
+import WorkOutlineIcon from '@mui/icons-material/WorkOutline';
+import CardGiftcardIcon from '@mui/icons-material/CardGiftcard';
+import AddBusinessIcon from '@mui/icons-material/AddBusiness';
+import HealthAndSafetyIcon from '@mui/icons-material/HealthAndSafety';
+import ForumIcon from '@mui/icons-material/Forum';
+import GridOnIcon from '@mui/icons-material/GridOn';
+import AddchartIcon from '@mui/icons-material/Addchart';
+import MapIcon from '@mui/icons-material/Map';
+import ShareIcon from '@mui/icons-material/Share';
+
 const Sidebar = () => {
     return (
         <>
@@ -24,6 +43,72 @@ const Sidebar = () => {
                             <div className="item">
                                 <DashboardIcon className="icon" />
                                 <p>Dashboard</p>
+                            </div>
+                        </div>
+                        <div className="title">
+                            <h5>Apps</h5>
+                            <div className="item">
+                                <CalendarTodayIcon className="icon" />
+                                <p>Calender</p>
+                            </div>
+                            <div className="item">
+                                <ChatBubbleOutlineIcon className="icon" />
+                                <p>Chat</p>
+                            </div>
+                            <div className="item">
+                                <MailOutlineIcon className="icon" />
+                                <p>Email</p>
+                            </div>
+                            <div className="item">
+                                <ContentPasteIcon className="icon" />
+                                <p>Tasks</p>
+                            </div>
+                            <div className="item">
+                                <BusinessCenterIcon className="icon" />
+                                <p>Projects</p>
+                            </div>
+                            <div className="item">
+                                <ImportContactsIcon className="icon" />
+                                <p>Contacts</p>
+                            </div>
+                        </div>
+                        <div className="title">
+                            <h5>Componets</h5>
+                            <div className="item">
+                                <PersonAddAlt1Icon className="icon" />
+                                <p>Base UI</p>
+                            </div>
+                            <div className="item">
+                                <NoteAddIcon className="icon" />
+                                <p>Widgets</p>
+                            </div>
+                            <div className="item">
+                                <ViewArrayIcon className="icon" />
+                                <p>Extended UI</p>
+                            </div>
+                            <div className="item">
+                                <WorkOutlineIcon className="icon" />
+                                <p>Icons</p>
+                            </div>
+                            <div className="item">
+                                <CardGiftcardIcon className="icon" />
+                                <p>Forms</p>
+                            </div>
+                            <div className="item">
+                                <AddBusinessIcon className="icon" />
+                                <p>Tables</p>
+                            </div>
+                            <div className="item">
+                                <HealthAndSafetyIcon className="icon" />
+                                <p>Charts</p>
+                            </div>
+                            <div className="item">
+                                <ForumIcon className="icon" />
+                                <p>Maps</p>
+                            </div>
+                            <div className="item">
+                                <GridOnIcon className="icon" />
+                                <p>Multi Level</p>
                             </div>
                         </div>
                     </div>
